ae1528adffa5c96b75dc688cff512064aada9867
Bartłomiej Kuras <hash.deb@gmail.com>
2026-08-20 23:42:34 +0200

    chore: update tracevault to 0.28.2

--- Formula/tracevault.rb
+++ Formula/tracevault.rb
@@ -1,28 +1,28 @@
 class Tracevault < Formula
   desc "CLI tool for AI code tracing and attribution"
   homepage "https://github.com/VirtusLab/visdom-ai-tracing-cli"
-  version "0.28.1"
+  version "0.28.2"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/VirtusLab/visdom-ai-tracing-cli/releases/download/v0.28.1/tracevault-v0.28.1-aarch64-apple-darwin.tar.gz"
-      sha256 "f51949cf8dd3587bce031874595609be62ad28e92f1d257e4bb459257faf6527"
+      url "https://github.com/VirtusLab/visdom-ai-tracing-cli/releases/download/v0.28.2/tracevault-v0.28.2-aarch64-apple-darwin.tar.gz"
+      sha256 "0f407e7fdc9ed45508c4c673810a0aee8cbd2eb48fa644f76c2145f178c90c69"
     end
     on_intel do
-      url "https://github.com/VirtusLab/visdom-ai-tracing-cli/releases/download/v0.28.1/tracevault-v0.28.1-x86_64-apple-darwin.tar.gz"
-      sha256 "61f9adeead660197b83db55c27b974d287a846857911548e97060ffe020c7185"
+      url "https://github.com/VirtusLab/visdom-ai-tracing-cli/releases/download/v0.28.2/tracevault-v0.28.2-x86_64-apple-darwin.tar.gz"
+      sha256 "67ff1f85ba3768235875eb471c0c1b606f61c0f81a6af4f75a40ca8b4d0ea4c4"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/VirtusLab/visdom-ai-tracing-cli/releases/download/v0.28.1/tracevault-v0.28.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "b6b2f8a58553b9be3e73c6341f182abbc47fbd851ef0d63a351842db83e5db1d"
+      url "https://github.com/VirtusLab/visdom-ai-tracing-cli/releases/download/v0.28.2/tracevault-v0.28.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "fc930152a224684afd4b56c7c94556dc7425dad15681a9c65ffa66877dccf938"
     end
     on_intel do
-      url "https://github.com/VirtusLab/visdom-ai-tracing-cli/releases/download/v0.28.1/tracevault-v0.28.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "fc8c67356b83a441270484b56d4777cfe2ea52a6141ba0aea7458b886f4a7a8e"
+      url "https://github.com/VirtusLab/visdom-ai-tracing-cli/releases/download/v0.28.2/tracevault-v0.28.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "0a97f156f42d9888c522923ad67b316725b1f231af0ef223749be55f6b43ea91"
     end
   end
 
